d335e87976a41c0fb620390c6016cdfc8d9316f4
iDanchy <65911925+doiminus@users.noreply.github.com>
2025-04-30 20:03:21 +0200

    Remove faulty 'frontend-server/frontend-server/frontend' folder

--- frontend-server/frontend-server/frontend/frontend-server/routes/auth/logout.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// prazna ruta
-router.post('/api/users/logout', (req, res) => {
-    res.status(200).json({ message: 'Logout route placeholder' });
-});
-
-module.exports = router;
